7a15c0b2bebbaa113ef87e802df0c3802c72a50e
ZayLpursuit <Zaylors@pursuit.org>
2022-11-27 22:07:19 -0500

    Liked videos working

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Routes,Route, } from "react-router-dom"
 import Nav from './Components/Nav';
 import About from './Components/About';
 import Home from './Components/Home';
-
+import LikedVideos from './Components/LikedVideos';
 import Video from './Components/Video';
+import { useState } from 'react';
 
 
 function App() {
+  const [likedVideos,setLikedVideos]=useState([])
  
   return (
     
@@ -17,8 +19,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about/" element={<About />}/>
-     <Route path="/video/:id" element={<Video  />} />
+     <Route path="/video/:id" element={<Video setLikedVideos={setLikedVideos} likedVideos={likedVideos}/>}/>
     <Route path="/category/:category" element={<Home/>} />
+    <Route path="/liked-videos" element={<LikedVideos likedVids={likedVideos}/>}/>
 
     </Routes>
   </Router>
